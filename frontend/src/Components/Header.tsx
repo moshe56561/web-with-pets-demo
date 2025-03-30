358dbb6db589284a4b7b28c3dffe8a805e3c18c2
moshe56561 <moshe56561@gmail.com>
2025-03-30 23:45:09 +0300

    first commit4

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
       <div className="absolute inset-0 headerImage bg-cover bg-center"></div>
       <div className="relative min-h-[90vh] text-white z-10">
         <div className="mt-0 sm:mt-10 flex items-center justify-center">
-          <img src="pawLogo.png" className="w-[150px] sm:w-[200px]" />
+          <img src="pawLogo.png" className="w-[170px] sm:w-[200px]" />
         </div>
         <div className="mt-0 sm:-mt-10 flex flex-col items-center text-center px-4">
           <h1 className="font-bold text-[2.5rem] md:text-[5rem] lg:text-[4.5rem] text-black sm:text-white">
