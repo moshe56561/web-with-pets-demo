95a63b488b5ee33dc59123adb4f7b20e7b41ab89
moshe56561 <moshe56561@gmail.com>
2025-03-30 23:41:53 +0300

    first commit3

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,16 +1,16 @@
 export default function Header() {
   return (
-    <header className="relative bg-[#def4fc] pt-6 px-4 sm:px-10">
+    <header className="relative bg-[#def4fc] pt-6 px-0 sm:px-10">
       <div className="absolute inset-0 headerImage bg-cover bg-center"></div>
       <div className="relative min-h-[90vh] text-white z-10">
-        <div className="-mt-10 sm:mt-10 flex items-center justify-center">
-          <img src="pawLogo.png" className="w-[200px]" />
+        <div className="mt-0 sm:mt-10 flex items-center justify-center">
+          <img src="pawLogo.png" className="w-[150px] sm:w-[200px]" />
         </div>
-        <div className="-mt-10 flex flex-col items-center text-center">
-          <h1 className="font-bold text-[3.5rem] md:text-[5rem] lg:text-[4.5rem] text-black sm:text-white">
+        <div className="mt-0 sm:-mt-10 flex flex-col items-center text-center px-4">
+          <h1 className="font-bold text-[2.5rem] md:text-[5rem] lg:text-[4.5rem] text-black sm:text-white">
             BRAND NAME
           </h1>
-          <h2 className="font-bold text-[1.5rem] text-black sm:text-white">
+          <h2 className="font-bold text-[1.25rem] sm:text-[1.5rem] text-black sm:text-white">
             PET GROOMING SALON
           </h2>
           <p className="text-black sm:text-white">AUSTIN, TEXAS</p>
